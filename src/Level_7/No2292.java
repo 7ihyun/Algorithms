f5fcd6f28226e8f02d9f571f381864d86bea630b
Kim-Jihyun1 <jh129047@gmail.com>
2022-09-21 21:01:45 +0900

    java

---
+++ src/Level_7/No2292.java
@@ -0,0 +1,11 @@
+package Level_7;
+
+import java.io.IOException;
+
+public class No2292 {
+    public static void main(String[] args) throws IOException {
+        /*
+        N이 속한 겹에 있는 수들은 최소 루트의 방 방문
+         */
+    }
+}
