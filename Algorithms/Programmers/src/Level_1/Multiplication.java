520304615911e9679880921730ea56f7ad1bcf1d
Kim-Jihyun1 <jh129047@gmail.com>
2022-10-27 21:35:18 +0900

    update

---
+++ Algorithms/Programmers/src/Level_1/Multiplication.java
@@ -0,0 +1,13 @@
+package Level_1;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Multiplication result = new Multiplication();
+
+        System.out.println("두 수의 곱 : " + result.solution(27, 19));
+    }
+    public static int solution(int num1, int num2) {
+        int result = num1 * num2;
+        return result;
+    }
+}
