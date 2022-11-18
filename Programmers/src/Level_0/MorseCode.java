cf14e14156e62117de8593fd0e0c42f3dc33ee0f
Kim-Jihyun1 <jh129047@gmail.com>
2022-11-18 22:08:16 +0900

    Programmers Lv.0 with Java

---
+++ Programmers/src/Level_0/MorseCode.java
@@ -0,0 +1,47 @@
+package Level_0;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MorseCode {
+    public static void main(String[] args) {
+        System.out.println(MorseCode.solution(".... . .-.. .-.. ---"));
+    }
+    public static String solution(String letter) {
+        String answer = "";
+
+        Map<String, String> map = new HashMap<>();
+        map.put(".-", "a");
+        map.put("-...", "b");
+        map.put("-.-.", "c");
+        map.put("-..", "d");
+        map.put(".", "e");
+        map.put("..-.", "f");
+        map.put("--.", "g");
+        map.put("....", "h");
+        map.put("..", "i");
+        map.put(".---", "j");
+        map.put("-.-", "k");
+        map.put(".-..", "l");
+        map.put("--", "m");
+        map.put("-.", "n");
+        map.put("---", "o");
+        map.put(".--.", "p");
+        map.put("--.-", "q");
+        map.put(".-.", "r");
+        map.put("...", "s");
+        map.put("-", "t");
+        map.put("..-", "u");
+        map.put("...-", "v");
+        map.put(".--", "w");
+        map.put("-..-", "x");
+        map.put("-.--", "y");
+        map.put("--..", "z");
+
+        String[] str = letter.split(" ");
+        for (String i : str) {
+            answer += map.get(i);
+        }
+        return answer;
+    }
+}
