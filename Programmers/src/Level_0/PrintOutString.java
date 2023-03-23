6d288a3a4f6e4f21959bd2779686ec0e2caaf701
Kim-Jihyun1 <jh129047@gmail.com>
2023-03-23 15:48:00 +0000

    Programmers Lv.0 with Java

---
+++ Programmers/src/Level_0/PrintOutString.java
@@ -0,0 +1,12 @@
+package Level_0;
+
+import java.util.Scanner;
+
+public class PrintOutString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    // 문자열 입력
+        String a = sc.next();
+
+        System.out.println(a);  // 출력
+    }
+}
